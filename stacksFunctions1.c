264c91aa194481f9c957d88939ea4da1148a0a63
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-05-09 09:41:23 -0500

    Update file stackFunction1

--- stacksFunctions1.c
+++ stacksFunctions1.c
@@ -1,26 +1,66 @@
 #include "monty.h"
 
+#include "monty.h"
+
 /**
- * push - Agrega un nodo al inicio de la pila
- * @head: Doble puntero al primer nodo de la pila
- * @n: Valor del nodo
+ * push - Insert a new entry into the table with the given name and value
  *
- * Return: El nuevo nodo agregado a la pila
+ * @stack: The stack pointer
+ * @n: Number of entries in the table
+ *
+ * Return: 0 if successful and -1 otherwise
  */
-stack_t *push(stack_t **head, int n)
+void push(stack_t **stack, int n)
 {
-    stack_t *new_node;
+    stack_t *new = malloc(sizeof(stack_t));
+    if (new == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-        return (NULL);
+    new->n = n;
+    new->prev = NULL;
+    new->next = *stack;
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
-    if (*head)
-        (*head)->prev = new_node;
-    *head = new_node;
+    if (*stack != NULL)
+        (*stack)->prev = new;
 
-    return (new_node);
+    *stack = new;
+}
+
+/**
+ * pall - print a list of entries in the table
+ *
+ * @stack: Pointer to the stack structures
+ *
+ * Returns: a list of entries in the table.
+ */
+void pall(stack_t *stack)
+{
+    while (stack != NULL)
+    {
+        printf("%d\n", stack->n);
+        stack = stack->next;
+    }
+}
+
+
+/**
+ * free_stack - print a list of entries in the table
+ *
+ * @stack: Pointer to the stack structures
+ *
+ * Return: a list of entries in the table
+ */
+void free_stack(stack_t **stack)
+{
+    stack_t *temp;
+
+    while (*stack != NULL)
+    {
+        temp = *stack;
+        *stack = (*stack)->next;
+        free(temp);
+    }
 }
